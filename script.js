0a8bd87bc40fba457654482b7c04e30f65c270cb
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2022-09-20 21:00:23 +0300

    function for start/stop buttons

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const dateDiv = document.getElementById("date");
 let watch = document.getElementById("watch");
 let watchBefore = window.getComputedStyle(watch, "::before");
 const numberDisplay = document.getElementById("numberSquare");
-const startSTopBtn=document.getElementsByClassName("start")[0]
+const startSTopBtn = document.getElementsByClassName("start")[0]
 const buttons = document.getElementsByClassName("btn");
-const actionBtns=document.getElementsByClassName("actbtn")
-const resetBtn=document.getElementsByClassName("actbtn")[1];
-let active=false;
+const actionBtns = document.getElementsByClassName("actbtn")
+const resetBtn = document.getElementsByClassName("actbtn")[1];
+let active = false;
 let intervalId;
 
 
@@ -21,8 +21,8 @@ displayDate();
 //EVENTS FOR BUTTONS
 Array.from(buttons).forEach((btn) => {
     btn.addEventListener("click", () => {
-       
-        btn.disabled=false
+
+        btn.disabled = false
         if (btn.innerHTML == "+") {
             let parsedNumber = parseInt(numberDisplay.innerHTML);
             parsedNumber += 1
@@ -40,4 +40,36 @@ Array.from(buttons).forEach((btn) => {
         }
 
 
-        })});
+    })
+});
+
+//action start/stop buttons
+function action() {
+    active = true
+    Array.from(buttons).forEach((btn) => {
+        btn.disabled = true;
+    })
+    startSTopBtn.innerHTML = "Stop";
+    intervalId = setInterval(() => {
+        let parsedNumber = parseInt(numberDisplay.innerHTML);
+        parsedNumber -= 1
+        if (parsedNumber <= 3) {
+            numberDisplay.style.color = "red";
+        }
+        if (parsedNumber < 0) {
+            numberDisplay.innerHTML = 0;
+            numberDisplay.style.color = "white"
+            inactive();
+            // numberDisplay.style.color="white";
+            // watch.style.setProperty('--animationToPseudoElement', 'none')
+            // clearInterval(intervalId);
+
+        } else {
+
+            watch.style.setProperty('--animationToPseudoElement', 'rotate 1s linear infinite')
+            numberDisplay.innerHTML = parsedNumber
+        }
+
+    }, 1000)
+
+}
