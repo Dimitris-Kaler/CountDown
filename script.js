7ce7d330e54e4b0d615fed332efba1dbc33b136e
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2022-09-20 20:58:33 +0300

    event for + - btns

--- script.js
+++ script.js
@@ -11,9 +11,33 @@ let active=false;
 let intervalId;
 
 
-
+///display DATE 
 function displayDate() {
     setInterval(() => { dateDiv.innerHTML = new Date() }, 1000)
 }
 
 displayDate();
+
+//EVENTS FOR BUTTONS
+Array.from(buttons).forEach((btn) => {
+    btn.addEventListener("click", () => {
+       
+        btn.disabled=false
+        if (btn.innerHTML == "+") {
+            let parsedNumber = parseInt(numberDisplay.innerHTML);
+            parsedNumber += 1
+            console.log(parsedNumber);
+            numberDisplay.innerHTML = parsedNumber
+        }
+        if (btn.innerHTML == "-") {
+            let parsedNumber = parseInt(numberDisplay.innerHTML);
+            parsedNumber -= 1
+            if (parsedNumber < 0) {
+                numberDisplay.innerHTML = 0;
+            } else {
+                numberDisplay.innerHTML = parsedNumber
+            }
+        }
+
+
+        })});
